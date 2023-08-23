2eeb009ad8ab36eac4b6c4664cd8fd9402ba3d30
Pedro de Souza Queiroga <pedrosqueiroga@gmail.com>
2023-08-23 04:13:04 -0300

    docker_tag_push.sh

---
+++ docker_tag_push.sh
@@ -0,0 +1,3 @@
+docker tag $IMAGE_SHA southamerica-east1-docker.pkg.dev/cinema-da-fundacao-pirata/cinema-da-fundacao-pirata-registry/cinema_da_fundacao_website_pirata-phoenix
+docker push southamerica-east1-docker.pkg.dev/cinema-da-fundacao-pirata/cinema-da-fundacao-pirata-registry/cinema_da_fundacao_website_pirata-phoenix
+
